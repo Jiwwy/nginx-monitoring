7b37a651581bb8f86f9e52bb495856ac54c1fba8
Jimmy <jiwwy_djfercho@hotmail.com>
2025-04-13 11:47:44 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,1 +1,17 @@
+const express = require('express');
+const app = express();
+const port = 3000;
 
+app.get('/', (req, res) => {
+  res.send('Hola pin final con NGINX!');
+});
+
+// Endpoint de métricas personalizadas
+app.get('/metrics', (req, res) => {
+  res.set('Content-Type', 'text/plain');
+  res.send('my_app_request_count 1\n');
+});
+
+app.listen(port, () => {
+  console.log(`App corriendo en http://localhost:${port}`);
+});
