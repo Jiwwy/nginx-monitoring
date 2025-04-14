6144182d1ef0b7986c47ede17c536e5f5c7396df
Jimmy <jiwwy_djfercho@hotmail.com>
2025-04-13 21:47:14 -0500

    Update server.js

--- app/server.js
+++ app/server.js
@@ -1,17 +1,32 @@
 const express = require('express');
+const client = require('prom-client');
 const app = express();
 const port = 3000;
 
+// Crear un contador de visitas
+const counter = new client.Counter({
+  name: 'node_app_requests_total',
+  help: 'Total requests',
+});
+
+// Middleware para contar las solicitudes
+app.use((req, res, next) => {
+  counter.inc();
+  next();
+});
+
+// Ruta de métricas para Prometheus
+app.get('/metrics', async (req, res) => {
+  res.set('Content-Type', client.register.contentType);
+  res.end(await client.register.metrics());
+});
+
+// Ruta principal
 app.get('/', (req, res) => {
   res.send('Hola pin final con NGINX!');
 });
 
-// Endpoint de métricas personalizadas
-app.get('/metrics', (req, res) => {
-  res.set('Content-Type', 'text/plain');
-  res.send('my_app_request_count 1\n');
-});
-
+// Arrancar el servidor
 app.listen(port, () => {
   console.log(`App corriendo en http://localhost:${port}`);
 });
